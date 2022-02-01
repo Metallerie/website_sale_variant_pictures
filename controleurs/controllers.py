c5bd748845b1ea535b12a2b464acaff6a47b0a45
Metallerie <98831730+Metallerie@users.noreply.github.com>
2022-02-01 17:48:47 +0100

    Add files via upload

--- controleurs/controllers.py
+++ controleurs/controllers.py
@@ -1,1 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
 
+# class WebsiteSaleVariantPictures(http.Controller):
+#     @http.route('/website_sale_variant_pictures/website_sale_variant_pictures/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/website_sale_variant_pictures/website_sale_variant_pictures/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('website_sale_variant_pictures.listing', {
+#             'root': '/website_sale_variant_pictures/website_sale_variant_pictures',
+#             'objects': http.request.env['website_sale_variant_pictures.website_sale_variant_pictures'].search([]),
+#         })
+
+#     @http.route('/website_sale_variant_pictures/website_sale_variant_pictures/objects/<model("website_sale_variant_pictures.website_sale_variant_pictures"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('website_sale_variant_pictures.object', {
+#             'object': obj
+#         })
